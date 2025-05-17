9b4db6181cf8fa9b370d4bb8b70a5d2b89d79a64
MKiessling88 <163688688+MKiessling88@users.noreply.github.com>
2025-05-17 12:10:15 +0200

    edit mobilbuttons

--- levels/level1.js
+++ levels/level1.js
@@ -1,8 +1,8 @@
 const level1 = new Level(
     [
-        new Goblin(),
-        new Goblin(),
-        new Goblin(),
+        // new Goblin(),
+        // new Goblin(),
+        // new Goblin(),
         new Endboss(2400),
     ],
     [
